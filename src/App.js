8c6444b2303a4e9d7001f9f937531b773c1ad92e
lhc219 <85996602+lhc219@users.noreply.github.com>
2021-07-28 09:45:35 +0900

    start
    react blog start

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const name = '곰곰동이';
+
+  return (
+    <div className="App">
+      <div className="">
+        <div className="black-nav">
+          <div> {name}의 개발 블로그</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
